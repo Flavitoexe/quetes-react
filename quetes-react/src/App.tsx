cc186bde4e040d8456ea624510df6b6717914caf
Flavio Grilli <flavio.grilli@ynov.com>
2026-09-03 16:36:21 +0200

    can change pokemons with buttons

--- quetes-react/src/App.tsx
+++ quetes-react/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
@@ -14,10 +15,18 @@ const pokemonList = [
 
 function App() {
 
+  const [pokemonName, setPokemonName] = useState("Bulbasaur")
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName)
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name")
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-      <PokemonCard pokemon={pokemonList[1]} />
+      <button onClick={() => setPokemonName("Bulbasaur")}>Bulbasaur</button>
+      <button onClick={() => setPokemonName("Mew")}>Mew</button>
+      <PokemonCard pokemon={pokemon} />
     </div>
   )
 }
