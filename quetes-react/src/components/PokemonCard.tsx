ce59a0136bbe37e1c26bf5e4dbb4788e498a89f6
Flavio Grilli <flavio.grilli@ynov.com>
2026-09-03 15:47:43 +0200

    props in PokemonCard.tsx

--- quetes-react/src/components/PokemonCard.tsx
+++ quetes-react/src/components/PokemonCard.tsx
@@ -1,25 +1,20 @@
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
+interface PokemonProps {
+  pokemon: {
+    name: string,
+    imgSrc?: string
+  }
+}
 
-function PokemonCard() {
-    const pokemon = pokemonList[0]
+function PokemonCard({pokemon}: PokemonProps) {
     return (
-    <figure>
-        {pokemon.imgSrc != null ? (
-            <img src={pokemon.imgSrc} alt={pokemon.name} />
-        ) : (
-            <p>???</p>
-        )}
-        <figcaption>{pokemon.name}</figcaption>
-    </figure>
+      <figure>
+          {pokemon.imgSrc != null ? (
+              <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+              <p>???</p>
+          )}
+          <figcaption>{pokemon.name}</figcaption>
+      </figure>
     )
 }
 
